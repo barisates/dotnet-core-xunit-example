077ddb2b3f8aab57d0034f403466fc201c535a62
bates <bates@trabilisim.com>
2019-11-26 22:06:54 +0300

    unit test project

---
+++ dotnet-core-xunit-test/UserControllerTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace dotnet_core_xunit_test
+{
+    public class UserControllerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
